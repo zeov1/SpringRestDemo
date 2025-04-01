681b2c5ee3d289f29f17cbd07fcdcd94359cd787
Sergey Nesterenko <47058928+zeov1@users.noreply.github.com>
2025-04-01 23:44:16 +0300

    Employee: CREATE

--- src/main/java/ru/zeovl/springrestdemo/services/EmployeeService.java
+++ src/main/java/ru/zeovl/springrestdemo/services/EmployeeService.java
@@ -1,14 +1,19 @@
 package ru.zeovl.springrestdemo.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import ru.zeovl.springrestdemo.models.Employee;
 import ru.zeovl.springrestdemo.repositories.EmployeeRepository;
 import ru.zeovl.springrestdemo.utils.EmployeeNotFoundException;
+import ru.zeovl.springrestdemo.utils.InvalidEmployeeException;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional(readOnly = true)
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
@@ -28,5 +33,21 @@ public class EmployeeService {
         } else {
             throw new EmployeeNotFoundException(id);
         }
+    }
+
+    @Transactional
+    public Employee save(Employee employee, BindingResult bindingResult) throws InvalidEmployeeException {
+        if (bindingResult.hasErrors()) {
+            StringBuilder message = new StringBuilder();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                message
+                        .append(error.getField())
+                        .append(": ")
+                        .append(error.getDefaultMessage())
+                        .append("; ");
+            }
+            throw new InvalidEmployeeException(message.toString());
+        }
+        return employeeRepository.save(employee);
     }
 }
